a9e3f5e1584dfe873b7267449b55f30bdc4896b6
loganfidler <loganfidler@gmail.com>
2019-12-09 09:22:42 -0700

    Basic table build functionality done

--- js/script.js
+++ js/script.js
@@ -6,18 +6,20 @@
 
 var rows, columns, startDate, tableStructure;
 
-function runProgram() {
+var getButton = document.getElementById("tableButton");
+console.log(getButton);
+getButton.addEventListener("click", runProgram);
+
+function runProgram(E) {
     //get rows and columns. 
-    rows = parseInt(document.getElementById("rowInput").value);
-    columns = parseInt(document.getElementById("columnInput").value);
+    rows = document.getElementById("rowInput").value;
+    columns = document.getElementById("columnInput").value;
     
     // create the table structure
     tableStructure = createStructure(rows, columns);
 
     // create the html table based on the table structure from user
-    //createTable(tableStructure);
-    console.log(tableStructure);
-    console.log(this);
+    createTable(tableStructure);
 }
 
 // create the structure needed for the dynamically generated table
@@ -31,10 +33,23 @@ function createStructure(rows, columns) {
             outputArray[i][j] = counter++;
         }
     }
-    
     return outputArray;
 }
 
 function createTable (tableStructure) {
     var selectTable = document.getElementById("calendarTable");
+
+    for (var i = 0; i < tableStructure.length; i++) {
+        var createTR = document.createElement("tr");
+
+        for (var j = 0; j < tableStructure[i].length; j++) {
+            var createTD = document.createElement("td");
+            var txt = document.createTextNode(tableStructure[i][j]);
+            createTD.appendChild(txt);
+            createTR.appendChild(createTD);
+        }
+
+        selectTable.appendChild(createTR);
+    }
+    
 }
